05bcbd90b04085e14441e89f4a2570a185e0bc0f
Naiem <jahid.hasan@monstar-lab.com>
2022-05-28 16:40:09 +0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by BD Macbook Air B on 28/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
