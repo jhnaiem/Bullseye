ccf408bc9712eb1b5f6552d59b673a301a189e50
Naiem <jahid.hasan@monstar-lab.com>
2022-05-31 12:59:46 +0600

    feat: Add color & gradient , ZStack

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,46 +14,53 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack{
-            Text("🎯🎯🎯 \nPut the bullseye as you can to".uppercased())
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center )
-                    .lineSpacing(4.0)
-                    .font(.footnote)
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("🎯🎯🎯 \nPut the bullseye as you can to".uppercased())
+                        .bold()
+                        .kerning(2.0)
+                        .multilineTextAlignment(.center )
+                        .lineSpacing(4.0)
+                        .font(.footnote)
 
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-                HStack {
-                    Text("1")
-                        .bold()
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .bold()
-                }
-                .padding()
-            
-                Button(action: {
-                    print("Hello, SwiftUI!")
-                    self.alertIsVisible = true
-                }) {
-                    Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                }
-                    .padding(20.0)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(21.0)
-                    .alert("Hello there!", isPresented: $alertIsVisible){
-                        Button("Awesome"){}
-                    }message: {
-                        let roundedValue = Int(sliderValue.rounded())
-                        Text("The slider value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round.")
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    HStack {
+                        Text("1")
+                            .bold()
+                        Slider(value: $sliderValue, in: 1.0...100.0)
+                        Text("100")
+                            .bold()
                     }
-            
+                    .padding()
+                
+                    Button(action: {
+                        print("Hello, SwiftUI!")
+                        self.alertIsVisible = true
+                    }) {
+                        Text("Hit me".uppercased())
+                            .bold()
+                            .font(.title3)
+                    }
+                        .padding(20.0)
+                        .background(ZStack {
+                            Color("ButtonColor")
+                            LinearGradient(colors: [Color.white.opacity(0.3),Color.clear], startPoint: .top, endPoint: .bottom)
+                        })
+                        .foregroundColor(Color.white)
+                        .cornerRadius(21.0)
+                        .alert("Hello there!", isPresented: $alertIsVisible){
+                            Button("Awesome"){}
+                        }message: {
+                            let roundedValue = Int(sliderValue.rounded())
+                            Text("The slider value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round.")
+                        }
+                
+            }
         }
     }
 }
